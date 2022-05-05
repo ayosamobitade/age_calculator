57747c8597b43c3ebff5c48c1e73e66d2961c1c0
UncleAyo <ayosamobitade@gmail.com>
2022-05-05 18:09:03 +0100

    now finally

--- agelator/age_calculate.py
+++ agelator/age_calculate.py
@@ -12,36 +12,34 @@ class birthday_calculator:
     #subtract today from the birthday_delta    
     # sum bugs up there
         pass
-    
-    
-
-    
+      
     def num_day_lived(self):
         # next let's see calculate the number of day spent
         today_date = datetime.date.today()
         birth_day = datetime.datetime.strptime(self.birth_date, '%Y-%m-%d')
         return (datetime.date.today() - birth_day.date()).days
+        
          
-    
-    
-    
     def num_hours_lived(self):
-        #return (int(self.num_day_lived())//24)
-        pass
+        # calculate the number of days lived
+        return f'{self.num_day_lived() * 24 :,}'
         
     def num_seconds_lived(self):
-        pass
+        # calculate the number of day lived
+        return  f'{self.num_day_lived() * 24 * 60 :,}'
         
-    def num_sleep_hour():
-        pass
+    def num_sleep_hour(self):
+        # calculate the number of hour spent sleeping
+        return f'{self.num_day_lived() * 24 // 0.33:,}'
         
-    def num_sleep_day():
-        pass
+    def num_sleep_day(self):
+        # calculate the number of dady spent sleeping
+        return f'{self.num_day_lived() // 0.33:,}'
         
         
         
     
     
-print(birthday_calculator("1991-05-14", 2).num_day_lived())
+
 
     
--- agelator/views.py
+++ agelator/views.py
@@ -7,20 +7,33 @@ from django.http import request
 
 def age_page(request):
     birthday_input = ""
-    time_input = ""
+    time_input = 0
     day_spent = ""
+    hour_spent=""
+    second_spent=""
+    sleep_day = ""
+    sleep_hour = ""
     if request.method == 'POST':
         if request.POST.get('age_submit'):
             birthday_input = request.POST['birth_day']
             time_input = request.POST['birth_time']
             agilator = age_calculate.birthday_calculator(birthday_input, time_input)
             day_spent = agilator.num_day_lived()
+            hour_spent = agilator.num_hours_lived()
+            second_spent=agilator.num_seconds_lived()
+            sleep_day = agilator.num_sleep_day()
+            sleep_hour = agilator.num_sleep_hour()
 
                         
         else:
             redirect('age_html')
             birthday_input = ""
             time_input = ""
+            day_spent = ""
+            hour_spent=""
+            second_spent=""
+            sleep_day = ""
+            sleep_hour = ""
     else:
         print("Error Occurred")
   
@@ -28,5 +41,9 @@ def age_page(request):
     {
      "birth_day" : birthday_input,
      "birth_time": time_input,
-     "day_spent" : day_spent
+     "day_spent" : f'{day_spent:,}',
+     "hour_spent":hour_spent,
+     "second_spent":second_spent,
+     "sleep_day" : sleep_day,
+     "sleep_hour": sleep_hour
     })
